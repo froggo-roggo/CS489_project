76f89722f6aaa0e9157d2d6c7584e5b483584476
froggo-roggo <hardrock6hsy@gmail.com>
2023-11-28 21:32:38 +0900

    update popupdetector

--- ChromeExtension/PopupDetector/popup.js
+++ ChromeExtension/PopupDetector/popup.js
@@ -6,7 +6,8 @@
 
 // get the button ID element
 let doFind = document.getElementById("doFind");
-var lst = [];
+let aud = new Audio("srcs/beep-06.mp3");
+let lst = [];
 
 // Onclick Event
 doFind.addEventListener("click", changeState);
@@ -19,26 +20,42 @@ doFind.addEventListener("click", changeState);
  * (4) (If possible) set the border of every button of the popup element
  **/
 
-function setPageAddSomething(lst) {
+function setPageAddSomething() {
   //document.body.prepend("This page has popup");
   //window.alert("Hmm");
-  console.log("Yahoo!");
+  var res = {};
+  var list = [];
+  var detected = false;
   let a = document.querySelectorAll('div');
   for (var e of a){
 	var zi = getComputedStyle(e).zIndex;
-	if (zi > 500){
-		console.log("Possibe Popup Content");
-		e.style.border = "5px solid #ff00ff";
-		var clst = e.childNodes;
-		console.log("child contents: "+clst.length);
+	var ew = parseInt(getComputedStyle(e).width);
+	var eh = parseInt(getComputedStyle(e).height);
+	//console.log(zi+" "+parseInt(ew)+" "+eh);
+	if ((zi > 500)){
+		//console.log(e);
+		if((ew > 128)&&(eh > 128)){
+			detected = true;
+			e.style.border = "5px solid #ff00ff";
+		}
+		var clst = e.children;
 		for(var c of clst){
-			if(((c.tagName) == "DIV")||((c.tagName) == "BUTTON")){
+			//console.log(typeof(c));
+			var cw = parseInt(getComputedStyle(c).width);
+			var ch = parseInt(getComputedStyle(c).height);
+			if((((c.tagName) == "DIV")||((c.tagName) == "BUTTON")||((c.tagName) == "A")) && (cw > 128)&&(ch > 128)){
+				detected = true;
 				c.style.border = "5px solid #ff00ff";
 			}
 		}
-		lst.push(e);
+		list.push(e);
+		
 	}
   }
+  res['reslist'] = list;
+  res['detres'] = detected;
+  //console.log(res);
+  return res;
 }
 
 /*
@@ -61,10 +78,27 @@ async function changeState(){
 	  chrome.scripting.executeScript({
 		target: { tabId: tab.id },
 		function: setPageAddSomething,
-		args: [lst],
+	  }).then(detectionResult => {
+		/*for (const {frameId, result} of detectionResult) {
+			console.log('Frame '+ frameId + ' result: ', result);
+		}*/
+		/*
+			The strucrue of detectionResult Object
+			Itself: Array(1)
+			0: dictionary
+				'documentId': '8EF0BF9AA5B95CFDD94EBE06BB132D0F']
+				'frameId': 0
+				'result': {detres: true, reslist: Array(2)}
+		*/
+		var detres = detectionResult[0]['result']['detres'];
+		//lst =  detectionResult[0]['result']['reslist'];
+		if (detres){
+			console.log("detected");
+			aud.play();
+			doFind.innerText = "Maybe there's popup";
+		}
 	  });
 	  await doFind.classList.add("selected");
-	  //doFind.innerText = "Back to original page";
   }
   /*else{
 	  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
